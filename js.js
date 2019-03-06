d274fcd7b482d1fe9c08aed3e78413ddbfa80fe1
Teigan H <teigan.hockman@gmail.com>
2019-03-06 09:34:55 -0500

    Front-end update

---
+++ js.js
@@ -0,0 +1,14 @@
+const score = document.querySelector('aside > div')
+const arrowPattern = [up, down, left, right]
+
+const upArrow = document.querySelector('.up-arrow')
+const leftArrow = document.querySelector('.left-arrow')
+const downArrow = document.querySelector('down-arrow')
+const rightArrow = document.querySelector('.right-arrow')
+
+const changeArrowColor = function() {
+    // When up arrow is called, up arrow changes color
+    // When down arrow is called, down arrow changes color
+    //when left arrow is called, left arrow changes color
+    //when right arrow is called right arrow changes color
+}
